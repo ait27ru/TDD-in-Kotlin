1fde0c1be36e4c175e8fb05cccdc85a6668a5709
ait27ru <ait27ru@gmail.com>
2024-02-21 21:30:43 +1300

    Refactor unit tests

--- src/test/kotlin/utils/CalculatorTest.kt
+++ src/test/kotlin/utils/CalculatorTest.kt
@@ -1,35 +1,53 @@
 package utils
 
-import org.junit.jupiter.api.Test
-import utils.Calculator
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
+// Naming convention used in this file: When_TestCondition_Expect_ExpectedOutcome
+// Another common naming convention: FunctionName_TestCondition_ExpectedOutcome
+
 class CalculatorTest {
+
+    private val calculator = Calculator()
+
     @Test
-    fun testDivide2By2() {
-        val calculator = Calculator()
-        val result = calculator.parse("2 / 2")
-        assertEquals(1, result)
+    // When using another naming convention, the name would be
+    // parse_DividingFourByTwo_ShouldReturnTwo, which is less readable.
+    fun `when dividing 4 by 2 expect 2`() {
+        val result = calculator.parse("4 / 2")
+        assertEquals(2, result)
     }
 
     @Test
-    fun testMultiply2By2() {
-        val calculator = Calculator()
+    fun `when dividing by 0 expect IllegalArgumentException`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.parse("4 / 0")
+        }
+    }
+
+    @Test
+    fun `when multiplying 2 and 2 expect 4`() {
         val result = calculator.parse("2 * 2")
         assertEquals(4, result)
     }
 
     @Test
-    fun testAdd2To2() {
-        val calculator = Calculator()
+    fun `when adding 2 and 2 expect 4`() {
         val result = calculator.parse("2 + 2")
         assertEquals(4, result)
     }
 
     @Test
-    fun testMinus2From2() {
-        val calculator = Calculator()
+    fun `when subtracting 2 from 2 expect 0`() {
         val result = calculator.parse("2 - 2")
         assertEquals(0, result)
+    }
+
+    @Test
+    fun `when unknown operation expect IllegalArgumentException`() {
+        assertThrows<IllegalArgumentException> {
+            calculator.parse("8 & 1")
+        }
     }
 }
