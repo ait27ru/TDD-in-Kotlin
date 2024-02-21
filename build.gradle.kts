60d9249dc0af4846faecc464a2d617effbe042ab
ait27ru <ait27ru@gmail.com>
2024-02-21 22:22:24 +1300

    Update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    application
+}
+
+application {
+    mainClass = "org.example.MainKt"
 }
 
 group = "org.example"
@@ -10,7 +15,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.22")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 tasks.test {
@@ -20,6 +26,18 @@ tasks.test {
         events("passed", "skipped", "failed")
     }
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Created-By"] = "Andrei T."
+        attributes["Main-Class"] = "org.example.MainKt"
+    }
+    configurations.compileClasspath.get().forEach {
+        from(zipTree(it.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
