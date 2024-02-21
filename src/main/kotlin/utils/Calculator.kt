77b411b9d18115a63042b3a2b1381f9bb3a6abb0
ait27ru <ait27ru@gmail.com>
2024-02-21 21:30:22 +1300

    Add check for division by zero

--- src/main/kotlin/utils/Calculator.kt
+++ src/main/kotlin/utils/Calculator.kt
@@ -5,7 +5,7 @@ class Calculator {
         val (a, op, b) = s.split(" ")
         return when (op) {
             "*" -> a.toInt() * b.toInt()
-            "/" -> a.toInt() / b.toInt()
+            "/" -> if(b.toInt() != 0) a.toInt() / b.toInt() else throw IllegalArgumentException("Division by 0")
             "+" -> a.toInt() + b.toInt()
             "-" -> a.toInt() - b.toInt()
             else -> throw IllegalArgumentException("Invalid operator")
